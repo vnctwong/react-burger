3c31a181d9501a78cac257771c2795a51cd6ea52
VWMAC <vincentkywong@gmail.com>
2019-10-17 23:13:55 -0700

    BurgerBuilder container - props split betwwen burger and controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <Layout >
-      burger
+      <BurgerBuilder />
     </Layout>
   );
 }
