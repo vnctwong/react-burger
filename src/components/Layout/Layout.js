7725111c12920cbd2acb4705135b9f14c77e1cd1
VWMAC <vincentkywong@gmail.com>
2019-10-17 23:19:16 -0700

    layout.css added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SiderDrawer, Backdrop</div>
 
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
 
