e8d904fbd075c077bb40c4631304f26435e2ea07
VWMAC <vincentkywong@gmail.com>
2019-10-17 23:30:57 -0700

    app.js now class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <Layout >
-      <BurgerBuilder />
-    </Layout>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout >
+          <BurgerBuilder />
+        </Layout >
+      </div>
+    );
+  }
 }
 
 export default App;
