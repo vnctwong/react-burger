1a889a453537730caa15c449eda13c61483313b6
VWMAC <vincentkywong@gmail.com>
2019-10-26 23:55:23 -0700

    layout renders properly

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar, SiderDrawer, Backdrop</div>
+    <main className={classes.Content}>
+      {props.children}
+    </main>
+  </Aux>
+);
+
+export default layout;
