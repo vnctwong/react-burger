2ce9970c6bde3d8ec4723a3abc7c33269e85b7c3
VWMAC <vincentkywong@gmail.com>
2019-10-27 21:15:55 -0700

    css margins in layout renders properly

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
-import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
-  state = {
-    ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2,
-    }
-  }
-
   render() {
     return (
-      <Aux>
-        <Burger ingredients={this.state.ingredients} />
+      <aux>
+        <div>Burger</div>
         <div>Build Controls</div>
-      </Aux>
+      </aux>
     );
   }
-};
+}
 
 export default BurgerBuilder;
