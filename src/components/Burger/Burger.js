e78fb0a1417fc1d0bd0b81424e06f4773a42c9be
VWMAC <vincentkywong@gmail.com>
2019-10-28 00:02:09 -0700

    fix syntax error

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -15,8 +15,7 @@ const burger = (props) => {
     <div className={classes.Burger}>
 
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom' />
 
     </div>
