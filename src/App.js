937c233aad5fec098e926464794f60dba2c9a2b2
VWMAC <vincentkywong@gmail.com>
2019-10-27 21:04:00 -0700

    reset for module problem with burger class again

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from '.';
 
 class App extends Component {
   render() {
     return (
       <div >
         <Layout>
-          <BurgerBuilder />
+          <p>test</p>
         </Layout>
       </div>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,6 @@ import classes from './Layout.css';
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SiderDrawer, Backdrop</div>
-
     <main className={classes.Content}>
       {props.children}
     </main>
