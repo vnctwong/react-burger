25ea1155ed2da60a42d1eff1b0d98a9b9ee2377f
VWMAC <vincentkywong@gmail.com>
2019-11-04 23:54:52 -0800

    toolbar compo added to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, SiderDrawer, Backdrop</div>
+    <Toolbar />
+
     <main className={classes.Content}>
       {props.children}
     </main>
