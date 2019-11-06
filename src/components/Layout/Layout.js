45f332b4136d0bd9dbf47e5d7b8d0270ca9a4da9
VWMAC <vincentkywong@gmail.com>
2019-11-05 16:01:37 -0800

    add logo to toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,6 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 const layout = (props) => (
   <Aux>
     <Toolbar />
-
     <main className={classes.Content}>
       {props.children}
     </main>
