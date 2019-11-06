c390adccd3b7ad55bc95ede86697b5ac25031217
VWMAC <vincentkywong@gmail.com>
2019-11-05 23:59:32 -0800

    add navigation item compo

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,13 @@
+const React from '.react';
+import classes from './NavigationItem.css';
+
+const navigationItem = (props) => (
+  <li className={classes.navigationItem}>
+    <a
+      href={props.link}
+      className={props.active ? classes.active : null}>
+      {props.children}</a>
+  </li>
+);
+
+export default navigationItem;
