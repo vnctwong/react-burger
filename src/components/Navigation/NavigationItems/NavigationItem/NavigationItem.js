b8bca57f8caf692ce5e78b34df2a7d026ef498d7
VWMAC <vincentkywong@gmail.com>
2019-11-06 23:40:54 -0800

    nav items renders in toolbar

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,8 +1,8 @@
-const React from '.react';
+import React from 'react';
 import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
-  <li className={classes.navigationItem}>
+  <li className={classes.NavigationItem}>
     <a
       href={props.link}
       className={props.active ? classes.active : null}>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,12 @@
-const React from 'react';
+import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
   <ul className={classes.navigationItems}>
-    <NavigationItem link='/' active>Burger Builder<NavigationItem />
-      <NavigationItem link='/'>Checkout<NavigationItem />
-
+    <NavigationItem link='/' active>Burger Builder</NavigationItem>
+    <NavigationItem link='/'>Checkout</NavigationItem>
   </ul>
-      )
-      
-      export default navigationItems;
+);
+
+export default navigationItems;
