c9043332e0ea458d5be9dbfc34e4365f51338e22
VWMAC <vincentkywong@gmail.com>
2019-11-07 21:43:47 -0800

    spelling err className css

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-  <ul className={classes.navigationItems}>
+  <ul className={classes.NavigationItems}>
     <NavigationItem link="/" active>Burger Builder</NavigationItem>
     <NavigationItem link="/">Checkout</NavigationItem>
   </ul>
