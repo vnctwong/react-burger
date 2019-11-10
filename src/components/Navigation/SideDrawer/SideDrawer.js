5b5576a6155fd228cc0be05aed409eca891c0016
VWMAC <vincentkywong@gmail.com>
2019-11-09 17:33:21 -0800

    backdrop reused in sideDrawer, no onClick yet

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,19 +3,24 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
   return (
-    <div className={classes.SideDrawer}>
+    <Aux>
+      <Backdrop show />
+      <div className={classes.SideDrawer}>
 
-      <div className={classes.Logo}>
-        <Logo />
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
