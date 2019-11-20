ff061bec5d267b2434ec39a5089e5bccd2bf77dd
VWMAC <vincentkywong@gmail.com>
2019-11-19 21:40:35 -0800

    add componentWillUnmount to errorHandler

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import Layout from '../src/hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+  state = {
+    show: true
+  }
+
   render() {
     return (
       <div >
         <Layout>
-          <BurgerBuilder />
+          {this.state.show ? <BurgerBuilder /> : null}
         </Layout>
       </div>
     );
