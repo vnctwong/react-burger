510f87c035889b38620b65f04842c708b379838e
VWMAC <vincentkywong@gmail.com>
2019-11-19 21:46:09 -0800

    refrac after test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div >
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <BurgerBuilder />
         </Layout>
       </div>
     );
