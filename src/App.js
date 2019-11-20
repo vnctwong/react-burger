ee76981bcd76d86a7cc4de6bd2161f6a939b6278
VWMAC <vincentkywong@gmail.com>
2019-11-19 21:45:23 -0800

    cleanup for errorHandler, module works as expected

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import Layout from '../src/hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  state = {
-    show: true
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ show: false })
-    }, 3000);
-  }
-
   render() {
     return (
       <div >
