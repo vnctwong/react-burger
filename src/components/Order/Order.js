cdfe8a2219ccd4f6974c8a7fc71e71939a87e8de
VWMAC <vincentkywong@gmail.com>
2019-12-06 17:06:13 -0800

    orders in nav makes req to server, err fetch works - req made, data not set up to be rendered yet

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = (props) => (
-  <div className={classes.order}>
+  <div className={classes.Order}>
     <p>Ingredients: Salad (1) </p>
     <p>Price: <strong>5.55</strong> </p>
   </div>
