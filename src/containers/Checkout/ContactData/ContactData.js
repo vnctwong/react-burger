cb196f32dfe3eaaad4d2e94a0699713276cf8c26
VWMAC <vincentkywong@gmail.com>
2020-02-01 23:58:25 -0800

    actions set up for purchase success/error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -42,15 +42,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-
-    axios.post('/orders.json', order)
-      .then(res => {
-        this.setState({ loading: false })
-        this.props.history.push('/');
-      })
-      .catch(error => {
-        this.setState({ loading: false })
-      });
   };
 
   checkValidity(value, rules) {
