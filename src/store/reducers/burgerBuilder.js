96a888e7df2ca8367d5870ac13a7b459a6d4cd65
VWMAC <vincentkywong@gmail.com>
2020-02-01 23:22:35 -0800

    burger init and error handling handled by redux

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -38,7 +38,8 @@ const reducer = (state = initialState, action) => {
     case actionTypes.SET_INGREDIENTS:
       return {
         ...state,
-        ingredients: action.ingredients
+        ingredients: action.ingredients,
+        error: false
       };
 
     case actionTypes.FETCH_INGREDIENTS_FAILED:
