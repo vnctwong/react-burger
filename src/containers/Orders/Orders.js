8c21adca1967d047e36aabbd883a318aacaf841e
VWMAC <vincentkywong@gmail.com>
2020-02-12 00:08:26 -0800

    err handling for loading error

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -5,6 +5,7 @@ import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
   componentDidMount() {
@@ -12,14 +13,18 @@ class Orders extends Component {
   }
 
   render() {
+    let orders = <Spinner />;
+    if (!this.props.loading) {
+      orders = this.props.orders.map(order => (
+        <Order
+          key={order.id}
+          ingredients={order.ingredients}
+          price={+order.price} />
+      ))
+    };
     return (
       <div>
-        {this.state.orders.map(order => (
-          <Order
-            key={order.id}
-            ingredients={order.ingredients}
-            price={+order.price} />
-        ))}
+        {orders}
       </div>
     );
   }
@@ -27,7 +32,8 @@ class Orders extends Component {
 
 const mapStateToProps = state => {
   return {
-    orders: state.order.orders
+    orders: state.order.orders,
+    loading: state.order.loading
   }
 }
 
@@ -37,4 +43,4 @@ const mapDispatchToProps = dispatch => {
   }
 };
 
-export default connect(null, mapDispatchToProps)(withErrorHandler(Orders, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
