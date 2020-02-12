cb97b839b9ddd3dc51b5b66e063f23aa3bd967b9
VWMAC <vincentkywong@gmail.com>
2020-02-11 23:40:12 -0800

    set up actions to recieve order history w/ redux

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -11,20 +11,7 @@ class Orders extends Component {
   }
 
   componentDidMount() {
-    axios.get('/orders.json')
-      .then(res => {
-        const fetchedOrders = [];
-        for (let key in res.data) {
-          fetchedOrders.push({
-            ...res.data[key],
-            id: key
-          });
-        }
-        this.setState({ loading: false, orders: fetchedOrders });
-      })
-      .catch(err => {
-        this.setState({ loading: false });
-      });
+
   }
 
   render() {
