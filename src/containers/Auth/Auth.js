aef567dbe47736189dc9fbc59b0b89b302f9a389
VWMAC <vincentkywong@gmail.com>
2020-02-15 23:59:31 -0800

    change checkValidation method for clarity

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -10,9 +10,9 @@ class Auth extends Component {
   state = {
     controls: {
       email: {
-        elementType: 'email',
+        elementType: 'input',
         elementConfig: {
-          type: 'text',
+          type: 'email',
           placeholder: 'Email address'
         },
         value: '',
@@ -42,17 +42,28 @@ class Auth extends Component {
   }
 
   checkValidity(value, rules) {
-    let isValid = [];
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
     if (rules.required) {
-      isValid.push(value.trim() !== '');
+      isValid = value.trim() !== '' && isValid;
     }
     if (rules.minLength) {
-      isValid.push(value.length >= rules.minLength);
+      isValid = value.length >= rules.minLength && isValid
     }
     if (rules.maxLength) {
-      isValid.push(value.length <= rules.maxLength);
+      isValid = value.length <= rules.maxLength && isValid
     }
-    return isValid.indexOf(false) === -1 ? true : false;
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+    }
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid
+    }
+    return isValid;
   }
 
   inputChangedHandler = (event, controlName) => {
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -49,17 +49,28 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = [];
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
     if (rules.required) {
-      isValid.push(value.trim() !== '');
+      isValid = value.trim() !== '' && isValid;
     }
     if (rules.minLength) {
-      isValid.push(value.length >= rules.minLength);
+      isValid = value.length >= rules.minLength && isValid
     }
     if (rules.maxLength) {
-      isValid.push(value.length <= rules.maxLength);
+      isValid = value.length <= rules.maxLength && isValid
     }
-    return isValid.indexOf(false) === -1 ? true : false;
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+    }
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid
+    }
+    return isValid;
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
