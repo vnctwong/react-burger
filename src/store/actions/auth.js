cc6658be3d18593d18809fd16ab82c7c5734562c
VWMAC <vincentkywong@gmail.com>
2020-02-25 00:00:17 -0800

    checking localstate for auth before sucess/fail

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,7 @@ export const authFail = (error) => {
 export const logout = () => {
   localStorage.removeItem('token');
   localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -58,6 +59,7 @@ export const auth = (email, password, isSignup) => {
         const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
         localStorage.setItem('token', response.data.idToken);
         localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -73,3 +75,18 @@ export const setAuthRedirectPath = (path) => {
     path: path
   }
 }
+
+export const checkAuthState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout())
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'))
+      if (expirationDate > new Date()) {
+
+      }
+      dispatch(authSuccess())
+    }
+  };
+};
